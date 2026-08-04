97c2cd821f4cd4457a81962bbfc203c8b1ad2df2
ayush <ayushtomar8624@gmail.com>
2026-08-05 00:46:49 +0530

    update work experience

--- src/constants/work.config.js
+++ src/constants/work.config.js
@@ -1,38 +1,36 @@
 import { ExternalLink, Link } from "lucide-react";
 
 export const WORK = [
-  /*
     {
-      title: "The Xiting Way",
-      role: "Full Stack Developer",
-      dates: "Jan 2024 - Oct 2024",
-      location: "Remote",
-      description: "Worked on freelance based full stack web and mobile app projects.",
-      image: "/images/professional/txw.jpg",
+      title: "Morgan Stanley",
+      role: "Backend Developer",
+      dates: "Oct 2025 - present",
+      location: "Bengaluru, India",
+      description: "Earning my paycheck",
+      image: "/images/professional/morgan-stanley-64px.jpg",
       mlh: "#",
       links: [
         {
           title: "Website",
-          href: "https://www.thexitingway.com",
+          href: "https://www.morganstanley.com",
           icon: <Link className="h-3 w-3" />,
         },
       ],
     },
-    {
-      title: "Mrotion Labs",
-      role: "Frontend Developer",
-      dates: "Jan 2023 - July, 2023",
-      location: "Chandigarh",
-      description: "Worked on ReactJS projects.",
-      image: "/images/professional/mrotion.png",
-      mlh: "#",
-      links: [
-        {
-          title: "Website",
-          href: "https://mrotion.com/",
-          icon: <Link className="h-3 w-3" />,
-        },
-      ],
-    },
-    */
+    // {
+    //   title: "Mrotion Labs",
+    //   role: "Frontend Developer",
+    //   dates: "Jan 2023 - July, 2023",
+    //   location: "Chandigarh",
+    //   description: "Worked on ReactJS projects.",
+    //   image: "/images/professional/mrotion.png",
+    //   mlh: "#",
+    //   links: [
+    //     {
+    //       title: "Website",
+    //       href: "https://mrotion.com/",
+    //       icon: <Link className="h-3 w-3" />,
+    //     },
+    //   ],
+    // },
 ];
